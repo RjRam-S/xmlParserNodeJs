5631ff94f8df0482d8b1d49d9027eb2727731735
RjRam-S <55981102+RjRam-S@users.noreply.github.com>
2019-09-30 16:13:29 +0530

    Add files via upload
    Lexer files for xml parser

---
+++ lexer.js
@@ -0,0 +1,124 @@
+class lexer{
+    constructor(token){
+        token? this.tolex = token : undefined;
+        this.makeVarDefault();
+    }
+
+    makeVarDefault(){
+        this.result = [];
+        this.currentIndex = 0;
+        this.gotIdentifier = false;
+        this.gotOpenTag = false;
+        this.gotOpenTagWClose = false;
+        this.gotCloseTag = false;
+        this.gotSelfClose = false;
+        this.identifier = undefined;
+    }
+
+    makeIdentifierDefault(){
+        this.gotIdentifier = false;
+        this.identifier = undefined;
+    }
+
+    makeCIDefault(){
+        this.currentIndex = 0;
+    }
+
+    match(str){
+        const portion = this.tolex.substring(this.currentIndex, str.length);
+        return portion==this.tolex;
+    }
+
+    move(index){
+        index ? this.currentIndex+=index : this.currentIndex++;
+        return this.currentIndex;
+    }
+
+    peek(index){
+        return index ? this.tolex.substring(this.currentIndex, index) : this.tolex.charAt(this.currentIndex);
+    }
+
+    eat(str){
+        return this.move(str.length);
+    }
+
+    lex(token){
+        this.tolex = token;
+        this.makeVarDefault();
+        var char;
+        while(this.tolex.length!=0){
+            char = this.peek();
+            this.processToken(char);
+        }
+        if(this.tolex.length==0){
+            return this.result;
+        }
+    }
+
+    processToken(char){
+        const withNextChar = this.peek(this.currentIndex+2);
+        if(char=='<'){ 
+            this.gotIdentifier = false;
+            if(withNextChar=='</'){
+                this.gotOpenTag = false;
+                this.gotOpenTagWClose = true;
+                this.move(2);
+                //this.result.push({type:'open_tag_character', closing: true, value:'</'});
+            }else{
+                this.gotOpenTag = true;
+                this.move()
+                //this.result.push({type:'open_tag_character', value:char});
+            }
+        }
+        else if(char=='>' && !this.gotSelfClose){
+            if( (this.gotOpenTag || this.gotOpenTagWClose) && this.gotIdentifier){
+                this.result.push({
+                    type: this.gotOpenTag ? 'tag_open' : 'tag_close',
+                    tag_name: this.identifier
+                });
+                this.makeIdentifierDefault();
+                this.makeCIDefault();
+                this.tolex = this.tolex.substring(this.splitIdx+1, this.tolex.length);
+            }else{
+                this.move();
+            }
+        }
+        else if(char=='/'){  
+            if(withNextChar=='/>'){
+                this.gotSelfCose = true;
+                this.result.push({type:'tag_selfclose', tag_name: this.identifier});
+                this.move(2);
+                this.makeIdentifierDefault();
+                this.makeCIDefault();
+            }else{
+                this.move();
+            }
+        }
+        else if(char.match(/[!@#$%^&*(),.?":{}|]/g)!=null){
+            this.move();
+        }
+        else if(char.match(/^[a-z0-9]+$/i)!=null && !this.gotIdentifier){
+            this.gotIdentifier = true;
+            const token = this.tolex;
+            let splitIdx = token.indexOf('>');
+            if(this.identifier){
+                splitIdx = this.gotOpenTagWClose ? token.indexOf('</'+this.identifier)+2 : token.indexOf('<'+this.identifier)+1;
+                splitIdx+=this.identifier.length;
+            } else{
+                splitIdx = token.indexOf('/>')!=-1 ? token.indexOf('/>') : splitIdx
+            }
+            const identifier = this.peek(splitIdx);
+            this.identifier = identifier;
+            //this.result.push({type:'identifier', value:identifier});
+            this.splitIdx = splitIdx;
+            //this.eat(identifier);
+        }
+        else{
+            this.move()
+        }
+    }
+}
+
+module.exports = {
+    lexer
+}
---
+++ lexer.spec.js
@@ -0,0 +1,110 @@
+const lex = require('./lexer');
+const lexer = new lex.lexer('raja');
+
+/*
+describe('Lexer basic functionalities', () => {
+    test('Match func', () => {
+        //Arrange
+        const expectedOutput = true;
+        //Act
+        const callingResult = lexer.match('four');
+        //Assert
+        expect(callingResult).toStrictEqual(expectedOutput);
+    });
+    test('Move func without index', () => {
+        //Arrange
+        //ci=0
+        const expectedOutput = 1;
+        //Act
+        const callingResult = lexer.move();
+        //Assert
+        expect(callingResult).toStrictEqual(expectedOutput);
+    });
+    test('Move func with index', () => {
+        //Arrange
+        //ci=1
+        const expectedOutput = 3;
+        //Act
+        const callingResult = lexer.move(2);
+        //Assert
+        expect(callingResult).toStrictEqual(expectedOutput);
+    });
+    test('Peek func without index', () => {
+        //Arrange
+        //ci=3
+        const expectedOutput = 'a';
+        //Act
+        const callingResult = lexer.peek();
+        //Assert
+        expect(callingResult).toStrictEqual(expectedOutput);
+    });
+    test('Peek func with index', () => {
+        //Arrange
+        //ci=3
+        lexer.currentIndex = 1;
+        const expectedOutput = 'aja';
+        //Act
+        const callingResult = lexer.peek(4);
+        //Assert
+        expect(callingResult).toStrictEqual(expectedOutput);
+    });
+    test('Eat func', () => {
+        //Arrange
+        //ci=1
+        const expectedOutput = 4;
+        //Act
+        const callingResult = lexer.eat('tri');
+        //Assert
+        expect(callingResult).toStrictEqual(expectedOutput);
+    });
+});
+*/
+
+describe('Lexer with input string', () => {
+    /*
+    test('lex "<tag>"', () => {
+        //Arrange
+        const expectedOutput = [{type: 'tag_open', tag_name: 'tag' }];
+        //Act
+        const callingResult = lexer.lex('<tag>');
+        //Assert
+        expect(callingResult).toMatchObject(expectedOutput);
+    });
+    test('lex "</tag>"', () => {
+        //Arrange
+        const expectedOutput = [{type: 'tag_close', tag_name: 'tag' }];
+        //Act
+        const callingResult = lexer.lex('</tag>');
+        //Assert
+        expect(callingResult).toMatchObject(expectedOutput);
+    });
+    test('lex "<tag/>"', () => {
+        //Arrange
+        const expectedOutput = [{type: 'tag_selfclose', tag_name: 'tag' }];
+        //Act
+        const callingResult = lexer.lex('<tag/>');
+        //Assert
+        expect(callingResult).toMatchObject(expectedOutput);
+    });
+    test('lex "<tag></tag>"', () => {
+        //Arrange
+        const expectedOutput = [{type: 'tag_open', tag_name: 'tag' },
+                                {type: 'tag_close', tag_name: 'tag' }];
+        //Act
+        const callingResult = lexer.lex('<tag></tag>');
+        //Assert
+        expect(callingResult).toMatchObject(expectedOutput);
+    });
+    */
+    test('lex "<catalog><book></book></catalog>"', () => {
+        //Arrange
+        const expectedOutput = [{type: 'tag_open', tag_name: 'catalog' },
+                                {type: 'tag_open', tag_name: 'book' },
+                                {type: 'tag_close', tag_name: 'book' },
+                                {type: 'tag_close', tag_name: 'catalog' }];
+        //Act
+        const callingResult = lexer.lex('<catalog><book></book></catalog>');
+        //Assert
+        expect(callingResult).toMatchObject(expectedOutput);
+    });
+});
